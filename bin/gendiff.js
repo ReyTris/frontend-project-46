018da362d5873b8f124cc169d319f83f3bca5695
unknown <nikita.samsonov@simbirsoft.com>
2023-01-26 14:34:52 +0300

    step-3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js'
 const program = new Command();
 
 program
@@ -9,4 +10,7 @@ program
     .option('-f, --format <type>', 'output format', 'stylish')
     .argument('<filepath1>')
     .argument('<filepath2>')
+    .action((filepath1, filepath2) => {
+        console.log(genDiff(filepath1, filepath2))
+    }), 
 program.parse();
